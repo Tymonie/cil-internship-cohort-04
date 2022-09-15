8cc7742c5497ddfb237a5560917cc1b22100acfb
emmabraboke <emmabraboke@gmail.com>
2022-09-15 11:39:48 +0100

    added python task files

---
+++ Emmanuel.Braboke/8-Python.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+def resize_image(image,width,height):
+    """
+     image is the image file path
+     width is the new width of the image
+     height is the new height of the imagw
+    """
+    img_file = Image.open(image)
+    #resized image
+    new_image = img_file.resize((width,height))
+
+    #save or copy image
+    new_image.save("new_image.jpg")
+
+
+resize_image("about1.jpg",100,100)
+

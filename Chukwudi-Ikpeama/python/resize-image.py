5801835840f7c9813727c8e90e539db0055cb355
chuxyz <ikpeamachukwudi@gmail.com>
2022-09-15 21:55:44 +0100

    restructure directory

--- Chukwudi-Ikpeama/python/resize-image.py
+++ Chukwudi-Ikpeama/python/resize-image.py
@@ -6,9 +6,9 @@ def resizeImage(imgSrc, **kwargs):
     height = kwargs['height'] # set image height
     # get image file name without extension
     fileName = os.path.splitext(imgSrc)[0] 
-    # append the width and height to resized copy
+    # append the width and height to the resized copy
     imgCopy = fileName + '_' + str(width) + 'x' + str(height) + '.jpg' 
-    img = Image.open(imgSrc) # Open image
+    img = Image.open(imgSrc) # get the image
     resizeImg = img.resize((width, height)) # resize image
     resizeImg.save(imgCopy) # save image
 
